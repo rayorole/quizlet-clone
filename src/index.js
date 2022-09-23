2fbdd9bc03c4d3dba888b99815acfc491a666a3e
Ray Orolé <orole.ray@protonmail.com>
2022-09-23 11:31:41 +0200

    Login page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
