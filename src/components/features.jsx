450677ed5450c20cf5388e5c5c26e05bd1be804c
Ray Orolé <orole.ray@protonmail.com>
2022-09-23 14:10:09 +0200

    Update aos

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -51,6 +51,39 @@ export default function Features() {
               className="block p-4 border border-gray-100 shadow-sm rounded-xl focus:outline-none focus:ring hover:border-gray-200 hover:ring-1 hover:ring-gray-200"
               href="/accountant"
               data-aos="fade-right"
+              data-aos-delay={100}
+            >
+              <span className="inline-block p-3 rounded-lg bg-gray-50">
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h6 className="mt-2 font-bold">Accountant</h6>
+
+              <p className="hidden sm:mt-1 sm:text-sm sm:text-gray-600 sm:block">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+
+            <a
+              className="block p-4 border border-gray-100 shadow-sm rounded-xl focus:outline-none focus:ring hover:border-gray-200 hover:ring-1 hover:ring-gray-200"
+              href="/accountant"
+              data-aos="fade-right"
+              data-aos-delay={200}
             >
               <span className="inline-block p-3 rounded-lg bg-gray-50">
                 <svg
@@ -113,6 +146,7 @@ export default function Features() {
               className="block p-4 border border-gray-100 shadow-sm rounded-xl focus:outline-none focus:ring hover:border-gray-200 hover:ring-1 hover:ring-gray-200"
               href="/accountant"
               data-aos="fade-right"
+              data-aos-delay={100}
             >
               <span className="inline-block p-3 rounded-lg bg-gray-50">
                 <svg
@@ -144,37 +178,7 @@ export default function Features() {
               className="block p-4 border border-gray-100 shadow-sm rounded-xl focus:outline-none focus:ring hover:border-gray-200 hover:ring-1 hover:ring-gray-200"
               href="/accountant"
               data-aos="fade-right"
-            >
-              <span className="inline-block p-3 rounded-lg bg-gray-50">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
-              <h6 className="mt-2 font-bold">Accountant</h6>
-
-              <p className="hidden sm:mt-1 sm:text-sm sm:text-gray-600 sm:block">
-                Lorem ipsum dolor sit amet consectetur.
-              </p>
-            </a>
-
-            <a
-              className="block p-4 border border-gray-100 shadow-sm rounded-xl focus:outline-none focus:ring hover:border-gray-200 hover:ring-1 hover:ring-gray-200"
-              href="/accountant"
-              data-aos="fade-right"
+              data-aos-delay={200}
             >
               <span className="inline-block p-3 rounded-lg bg-gray-50">
                 <svg
