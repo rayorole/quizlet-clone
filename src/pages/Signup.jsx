7770c05ab8886de8e40aef0c0de5129a2ec8c445
Alexander Mannaerts <AlexanderM507@lln.campussintursula.be>
2022-09-23 13:44:53 +0200

    login

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -30,7 +30,7 @@ export default function Signup() {
             </a>
 
             <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to Quizlet Clone
+              Welcome to Quizzie
             </h1>
 
             <p className="mt-4 leading-relaxed text-gray-500">
@@ -114,21 +114,6 @@ export default function Signup() {
                 </button>
               </div>
 
-              <div className="col-span-6">
-                <p className="text-sm text-gray-500">
-                  By creating an account, you agree to our{' '}
-                  <a href="#" className="text-gray-700 underline">
-                    terms and conditions
-                  </a>{' '}
-                  and
-                  <a href="#" className="text-gray-700 underline">
-                    {' '}
-                    privacy policy
-                  </a>
-                  .
-                </p>
-              </div>
-
               <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                 <button className="inline-block px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 transition rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
                   Create an account
@@ -136,9 +121,9 @@ export default function Signup() {
 
                 <p className="mt-4 text-sm text-gray-500 sm:mt-0">
                   Already have an account?
-                  <a href="#" className="text-gray-700 underline">
-                    Log in
-                  </a>
+                  <button className="inline-block px-6 py-3 ml-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 transition rounded-md shrink-0 hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                    Login
+                  </button>
                   .
                 </p>
               </div>
