819c2c2d1bc55c366d6500af069f9112e21c5eae
Ray Orolé <orole.ray@protonmail.com>
2022-09-23 14:14:40 +0200

    Cashicon

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
+import { CashIcon } from '@heroicons/react/outline';
+
 export default function Features() {
   useEffect(() => {
     Aos.init({
-      offset: 200,
+      offset: 100,
       duration: 600,
       easing: 'ease-in-sine',
-      delay: 100,
+      delay: 50,
     });
   }, []);
   return (
@@ -22,22 +24,7 @@ export default function Features() {
               href="/accountant"
             >
               <span className="inline-block p-3 rounded-lg bg-gray-50">
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
+                <CashIcon className="w-6 h-6" />
               </span>
 
               <h6 className="mt-2 font-bold">Free</h6>
