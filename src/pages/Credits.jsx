bd123721b34b22d4ceb343896104955c9d8df3e8
Alexander Mannaerts <Alexander.Mannaert@hotmail.com>
2022-09-24 18:46:02 +0200

    credits

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React from 'react';
 import Nav from '../components/nav';
 import Footer from '../components/footer';
 
@@ -9,28 +7,23 @@ export default function Features() {
     <div>
       <Nav />
       <section className="bg-gray-50">
-        <div className="ml-250 py-32 mx-auto max-w-screen-xl ">
-          <div className="grid grid-cols-2 gap-10 sm:grid-cols-3">
-            <div className="block p-6 border cursor-default text-neutral-700 border-gray-300 rounded-xl">
-              <h7 className="mt-2 font-bold"> (cringe femboy) </h7>
-              <h6 className="mt-2 font-bold">Ray Orole</h6>
-              <br></br>
-              <h7 className="mt-2 font-bold">
-                {' '}
-                -main coder and database builder
-              </h7>
+        <div className="flex gap-10 pl-80 pt-24 ">
+          <div className=" p-6 border text-neutral-700 border-gray-300 rounded-xl">
+            <h7 className="mt-2 font-bold"> (cringe) </h7>
+            <h6 className="mt-2 font-bold">Ray Orole</h6>
+            <br></br>
+            <h7 className="mt-2 font-bold">-main coder and database builder</h7>
 
-              <img src="ray.png" width={350} />
-            </div>
+            <img src="ray.png" width={350} />
+          </div>
 
-            <div className="block p-6 border cursor-default text-neutral-700 border-gray-300 shadow-sm rounded-xl">
-              <h8 className="mt-2 font-bold"> (giga chad) </h8>
-              <h6 className="mt-2 font-bold">Alexander Mannaerts</h6>
-              <br></br>
-              <h7 className="mt-2 font-bold"> -the main idea and gui</h7>
+          <div className=" p-6 border text-neutral-700 border-gray-300 shadow-sm rounded-xl">
+            <h8 className="mt-2 font-bold"> (giga chad) </h8>
+            <h6 className="mt-2 font-bold">Alexander Mannaerts</h6>
+            <br></br>
+            <h7 className="mt-2 font-bold"> -the main idea and gui</h7>
 
-              <img src="alexander.png" width={350} />
-            </div>
+            <img src="alexander.png" width={350} />
           </div>
         </div>
       </section>
