2c346f195a92d8de38fd5c95b4d648ea0d1381cc
Ray Orolé <orole.ray@protonmail.com>
2022-09-24 02:34:07 +0200

    Update login, TODO: password validation css warn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,16 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   GithubAuthProvider,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { auth } from '../firebase';
 
+import { useRef } from 'react';
+
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
   const signInWithGoogle = async () => {
     signInWithPopup(auth, new GoogleAuthProvider())
       .then((res) => {
@@ -22,6 +28,19 @@ export default function Login() {
       })
       .catch((err) => alert('A login error occured'));
   };
+
+  const signInWithMail = async () => {
+    signInWithEmailAndPassword(
+      auth,
+      email.current.value,
+      password.current.value
+    )
+      .then((res) => {
+        window.location.replace('/sets');
+      })
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -53,10 +72,6 @@ export default function Login() {
           </div>
           <form className="mt-8 space-y-6" action="#" method="POST">
             <div>
-              <label className="block text-sm font-medium text-gray-700 mb-3">
-                Continue via Email:
-              </label>
-              <input type="hidden" name="remember" defaultValue="true" />
               <div className="-space-y-px rounded-md shadow-sm">
                 <div>
                   <label htmlFor="email-address" className="sr-only">
@@ -65,11 +80,27 @@ export default function Login() {
                   <input
                     id="email-address"
                     name="email"
+                    ref={email}
                     type="email"
                     autoComplete="email"
                     required
-                    className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     placeholder="Email address"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="sr-only">
+                    Password
+                  </label>
+                  <input
+                    id="password"
+                    ref={password}
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    placeholder="Password"
                   />
                 </div>
               </div>
@@ -128,7 +159,8 @@ export default function Login() {
 
             <div>
               <button
-                type="submit"
+                type="button"
+                onClick={signInWithMail}
                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 transition-all duration-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
