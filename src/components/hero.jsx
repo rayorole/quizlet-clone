64077e8fb4b0bd19dcadb3d3c04e3a47a50e340d
Ray Orolé <orole.ray@protonmail.com>
2022-10-27 14:40:28 +0200

    Fetch docs from Firestore

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -33,9 +33,9 @@ export default function Hero() {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M17 8l4 4m0 0l-4 4m4-4H3"
                   />
                 </svg>
